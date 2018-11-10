d68e6d5ade158327c8e11dc035d88a40ace5369b
jackforez <jackforez@gmail.com>
2018-11-11 00:59:34 +0700

    Update resources/views/page/layout/header.blade.php

--- resources/views/page/layout/header.blade.php
+++ resources/views/page/layout/header.blade.php
@@ -33,7 +33,7 @@
                   <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
                      <i class="zmdi zmdi-search"></i>
                   </div>
-                  <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="9">
+                  <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="{{Cart::total()}}">
                      <i class="zmdi zmdi-shopping-cart"></i>
                   </div>
                   <a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
